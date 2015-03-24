a1198c72bd7a3f84edecd3d9cc48575b903316fc
Miguel Barrientos <mbarrientos@lcc.uma.es>
2015-03-24 15:44:48 +0100

    FEATURE(dashboard): - uncomplete - Added AngularJS support (navbar, wizard navigation and wizard-header) - fixed some css bugs

---
+++ dashboard/src/main/webapp/app.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014 SeaClouds
+ * Contact: dev@seaclouds-project.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var app = angular.module('dashboard', []);
+
+app.directive('navbar', function () {
+    return {
+        restrict: 'EA',
+        replace: true,
+        templateUrl: 'views/navbar.html'
+    };
+});
+
+app.directive('wizardHeader', function(){
+    return {
+        restrict: 'EA',
+        replace: true,
+        templateUrl: 'views/wizard-header.html'
+    };
+});
+
+app.controller('wizardController',['$scope', function($scope){
+
+    $scope.currentStep = 1;
+
+    $scope.isSelected = function(step){
+        return $scope.currentStep == step;
+    };
+
+    $scope.previousStep = function(){
+        if($scope.currentStep != 1){
+            $('#panel-step-' + $scope.currentStep).addClass('hidden');
+            $scope.currentStep--;
+            $('#panel-step-' + $scope.currentStep).removeClass('hidden');
+        }
+    };
+
+    $scope.nextStep = function(){
+        if ($scope.currentStep != 5) {
+            $('#panel-step-' + $scope.currentStep).addClass('hidden');
+            $scope.currentStep++;
+            $('#panel-step-' + $scope.currentStep).removeClass('hidden');
+        }
+    };
+
+}]);
